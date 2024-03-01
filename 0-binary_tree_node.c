cad0722019f97d5128f8353856b6c3122c8be827
enejivk <enejivic@gmail.com>
2024-03-01 09:33:45 +0100

    Creating a New Node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,6 +8,10 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = GetNewNode(value, parent);
+    if(new == NULL)
+    {
+        return NULL;
+    }
 
 	return (new);
 }
