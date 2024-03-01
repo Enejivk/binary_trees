cba29f954a14e7aa0ba233ade97cdde492187a17
enejivk <enejivic@gmail.com>
2024-03-01 01:11:59 +0100

    Creating a binary

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    if(parent == NULL)
+    {
+        parent = GetNewNode(value);
+    }
+    return parent;
+}
+
+binary_tree_t* GetNewNode(int value)
+{
+    binary_tree_t *NewNode;
+    NewNode = (binary_tree_t *) malloc (sizeof(binary_tree_t));
+    NewNode->left = NULL;
+    NewNode->right = NULL;
+    NewNode->n = value;
+
+    return NewNode;
+}
